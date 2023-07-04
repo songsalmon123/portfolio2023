5e145044231b56d6a8b853c87d64dc8b7a2b0b05
songsalmon123 <129369136+songsalmon123@users.noreply.github.com>
2023-07-04 16:57:36 +0900

    230704

--- project003/js/ex001.js
+++ project003/js/ex001.js
@@ -1,114 +1,116 @@
 $(document).ready(function(){
 
-    // nav li를 클릭했을때, scroll의 위치가 변경된다.
-    // nav li를 클릭했을때, 각 li의 순번을 찾고 그 순번에 맞게 scroll의 위치를 매번 변경해라.
+    // nav li 를 클릭했을때,scroll의 위치가 바뀐다.
+    // nav li 를 클릭했을때,각li의 순번을 찾고 그 순번에 맞게 
+    // scroll의 위치를 매번 바뀌게 해라.
 
     $('nav li').click(function(){
-
         let i = $(this).index();
-        let sht = $('section').height();
+        let sht= $('section').height();
 
-        $('html, body').stop().animate({scrollTop:(i+1)*sht},1200);
+        $('html,body').stop().animate({scrollTop:(i+1)*sht},1200)
         $('nav li').removeClass('on')
         $(this).addClass('on')
 
-    })
+
+    });
 
     $('h1').click(function(){
 
-        // logo를 클릭했을때, scroll의 위치를 0으로 이동해라.
-        $('html, body').stop().animate({scrollTop:0},1200);
+        // logo를 클릭했을때 scroll의 위치를 0으로 이동해라.
+        $('html,body').stop().animate({scrollTop:0},1200);
         $('nav').fadeOut()
-        $('nav li').removeClass('on')   
-        // ^ 로고 눌렀을때 빤갈색 바가 사라지면서 메인페이지
-    })
+        $('nav li').removeClass('on')
+    });
 
     $('.wel').click(function(){
-        let sht = $('section').height();
-        $('html, body').stop().animate({scrollTop:sht},1200);
-        $('nav').fadeIn(1800)
+        let sht= $('section').height();
+        $('html,body').stop().animate({scrollTop:sht},1200);
+        $('nav').fadeIn(1800);
 
         $('nav li').removeClass('on')
         $('nav li').eq(0).addClass('on')
-        // ^로고를 누르고 웰컴 눌렀을때 네비가 첫번째로 변경되어있는것
+    });
 
-    })
 
     $(window).scroll(function(){
 
-        // 스크롤의 위치값을 확인하고 스크롤의 값이 section의 높이만큼 내려왔을때부터
-        // nav가 보여라.
-
+        // 스크롤의 위치값을 확인하고 스크롤의 값이 섹션의 높이만큼 내려왔을때
+        // 부터 nav가 보여라 
 
         let sc = $(this).scrollTop()
-        let sht = $('section').height();
+        let sht= $('section').height();
 
         // if(sc>=sht){
 
         //     $('nav').fadeIn(600);
-        // }
-        // else{
+        // }else{
         //     $('nav').fadeOut(600);
         // }
 
 
-
-
         if(sc>=sht*0 && sc<sht*1){
-
             $('nav').fadeOut(600);
-            $('nav li').removeClass('on')
+            $('nav li').removeClass('on') 
+
         }
-
-        // // 1
         // if(sc>=sht*1 && sc<sht*2){
-
         //     $('nav').fadeIn(600);
-        //     $('nav li').removeClass('on')
+        //     $('nav li').removeClass('on') 
         //     $('nav li').eq(0).addClass('on')
+  
+
         // }
 
-        // // 2
         // if(sc>=sht*2 && sc<sht*3){
-
         //     $('nav').fadeIn(600);
-        //     $('nav li').removeClass('on')
+        //     $('nav li').removeClass('on') 
         //     $('nav li').eq(1).addClass('on')
-        // }
+  
 
-        // // 3
+        // }
         // if(sc>=sht*3 && sc<sht*4){
-
         //     $('nav').fadeIn(600);
-        //     $('nav li').removeClass('on')
+        //     $('nav li').removeClass('on') 
         //     $('nav li').eq(2).addClass('on')
+  
+
         // }
 
-        // // 4
         // if(sc>=sht*4 && sc<sht*5){
-
         //     $('nav').fadeIn(600);
-        //     $('nav li').removeClass('on')
+        //     $('nav li').removeClass('on') 
         //     $('nav li').eq(3).addClass('on')
+  
+
         // }
 
-        
-    // 위의 사항을 for문으로 만들기
 
-    for(var a=0; a<4; a++){
 
-            
-        if(sc>=sht*(a+1) && sc<sht*(a+2)){
+        // 위의 사항을 for문으로 만들기
+
+        for(var a =0; a<4; a++){
+
+               if(sc>=sht*(a+1) && sc<sht*(a+2)){
             $('nav').fadeIn(600);
-            $('nav li').removeClass('on')
+            $('nav li').removeClass('on') 
             $('nav li').eq(a).addClass('on')
         }
+
     }
 
 
 
-    })
 
+
+
+
+
+
+
+
+
+    })
 
 
 })
---
+++ project003/js/ex002.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+// 마우스가 움직이는 값을 찾아라
+
+$(window).mousemove(function(e){
+
+    let x= e.pageX;
+    let y= e.pageY;
+
+    $('.cursor').css({'left':x, 'top':y})
+})
+
+// cursor가 em안에 들어갔을때, cursor의 보더색이 바뀌어라
+
+$('.main p em').mouseenter(function(){
+
+    $('.cursor').addClass('style1')
+    
+})
+$('.main p em').mouseleave(function(){
+
+    $('.cursor').removeClass('style1')
+    
+})
+
+
+$('.main p span').mouseenter(function(){
+
+    $('.cursor').addClass('style2')
+    
+})
+$('.main p span').mouseleave(function(){
+
+    $('.cursor').removeClass('style2')
+    
+})
+
+
+})
