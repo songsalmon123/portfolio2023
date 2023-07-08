0495b7dd364b2e72b369378fed08a4e3c7cd0761
songsalmon123 <songda3807@gmail.com>
2023-07-08 19:54:18 +0900

    230708

--- project003/js/javascript.js
+++ project003/js/javascript.js
@@ -5,5 +5,42 @@ $(document).ready(function(){
 
         $('.popUp').css({'display':'none'})
     })
+    
+
+
 })
 
+const videoContainer = document.querySelector('.video-container');
+const videoSlider = document.querySelector('.video-slider');
+const videoSlides = document.querySelectorAll('.video-slide');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let currentIndex = 0;
+
+function showSlide(index) {
+  videoSlider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function slidePrev() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = videoSlides.length - 1;
+  }
+  showSlide(currentIndex);
+}
+
+function slideNext() {
+  currentIndex++;
+  if (currentIndex >= videoSlides.length) {
+    currentIndex = 0;
+  }
+  showSlide(currentIndex);
+}
+
+prevBtn.addEventListener('click', slidePrev);
+nextBtn.addEventListener('click', slideNext);
+
+
+
+
+  
