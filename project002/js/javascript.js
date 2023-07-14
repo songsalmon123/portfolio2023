6354049568b04d7f21adb2edc2524d38a9b3259a
songsalmon123 <129369136+songsalmon123@users.noreply.github.com>
2023-07-14 16:57:11 +0900

    230714

--- project002/js/javascript.js
+++ project002/js/javascript.js
@@ -1,5 +1,23 @@
-$(document).raedy(function(){
+$(document).ready(function(){
 
+
+    $(window).scroll(function(){
+
+        let sc = $(this).scrollTop();
+        let aot = $('#about').offset().top
+        console.log(aot)
+  
+        if(sc>=aot){
+          
+          $('.hero').addClass('fix')
+  
+  
+        }
+        else{
+          $('.hero').removeClass('fix')
+        }
+  
+      })
 
     
 })
