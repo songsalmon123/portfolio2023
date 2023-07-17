0529e1be1802d09cd82acc6d358f348dcd176c6c
songsalmon123 <129369136+songsalmon123@users.noreply.github.com>
2023-07-17 16:56:10 +0900

    230717

--- project002/js/javascript.js
+++ project002/js/javascript.js
@@ -22,3 +22,46 @@ $(document).ready(function(){
       
     
 })
+document.addEventListener("DOMContentLoaded", function() {
+  var slideshow = document.getElementById("slideshow");
+  var images = slideshow.getElementsByTagName("img");
+  var previousButton = slideshow.querySelector(".previous");
+  var nextButton = slideshow.querySelector(".next");
+  var currentImage = 0;
+
+  function showImage(index) {
+    for (var i = 0; i < images.length; i++) {
+      images[i].classList.remove("active");
+    }
+
+    images[index].classList.add("active");
+  }
+
+  previousButton.addEventListener("click", function() {
+    currentImage = (currentImage - 1 + images.length) % images.length;
+    showImage(currentImage);
+  });
+
+  nextButton.addEventListener("click", function() {
+    currentImage = (currentImage + 1) % images.length;
+    showImage(currentImage);
+  });
+
+});
+
+function count(type)  {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result');
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+  
+  // 더하기/빼기
+  if(type === 'plus') {
+    number = parseInt(number) + 1;
+  }else if(type === 'minus')  {
+    number = parseInt(number) - 1;
+  }
+  
+  // 결과 출력
+  resultElement.innerText = number;
+}
